7254caf995992d816161198dde25a9ab33d9a3b1
Swathipalanisamy10 <swathipalanisamy04@gmail.com>
2026-03-02 17:25:28 +0530

    db.php , script.js ,style.css , and view-messages.php

--- db.php
+++ db.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $host = "localhost";
 $user = "root";
@@ -11,17 +10,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
-=======
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "cake";
 
-$conn = new mysqli($host, $user, $pass, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
->>>>>>> e79dea0f365ff9665af8e4a165b203b458e7321d
