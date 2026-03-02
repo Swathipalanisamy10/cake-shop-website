8d10034c3e25d4c634e28e0ae2082ecc2b83434c
Swathipalanisamy10 <swathipalanisamy04@gmail.com>
2026-03-02 16:48:15 +0530

    Updated login, logout and messages pages

--- logout.php
+++ logout.php
@@ -1,14 +1,13 @@
-<<<<<<< HEAD
+
 <?php
 session_start();
 session_destroy();
 header("Location: login.html");
 exit();
-=======
-<?php
+
 session_start();
 session_destroy();
 header("Location: login.html");
 exit();
->>>>>>> e79dea0f365ff9665af8e4a165b203b458e7321d
+
 ?>
